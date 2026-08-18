0859c6c7549496567a4ad72162efe077f7187aac
Black-Ace-Gr <gregorydavid373@gmail.com>
2026-08-18 10:07:00 +0300

    fixing urls

--- farms/urls.py
+++ farms/urls.py
@@ -1,116 +1,13 @@
-from django.contrib import messages
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import path
 
-from .forms import FarmForm
-from .models import Farm
+from . import views
 
+app_name = "farms"
 
-@login_required
-def create_farm(request):
+urlpatterns = [
 
-    if Farm.objects.filter(owner=request.user).exists():
-        messages.info(request, "You already have a farm.")
-        return redirect("farms:my_farm")
+    path("create/",views.create_farm,name="create_farm"),
+    path("my-farm/",views.my_farm,name="my_farm"),
+    path("edit/",views.edit_farm,name="edit"),
 
-    if request.method == "POST":
-
-        form = FarmForm(
-            request.POST,
-            request.FILES
-        )
-
-        if form.is_valid():
-
-            farm = form.save(commit=False)
-            farm.owner = request.user
-            farm.save()
-
-            messages.success(
-                request,
-                "Farm created successfully."
-            )
-
-            return redirect("farms:my_farm")
-
-        else:
-
-            messages.error(
-                request,
-                "Please fix the errors below and resubmit."
-            )
-
-    else:
-
-        form = FarmForm()
-
-    return render(
-        request,
-        "farms/create_farm.html",
-        {
-            "form": form
-        }
-    )
-
-@login_required
-def my_farm(request):
-    farm = Farm.objects.filter(owner=request.user).first()
-
-    if not farm:
-        return redirect("farms:create_farm")
-
-    return render(
-        request,
-        "farms/my_farm.html",
-        {
-            "farm": farm
-        }
-    )
-
-@login_required
-def edit_farm(request):
-
-    farm = get_object_or_404(
-        Farm,
-        owner=request.user
-    )
-
-    if request.method == "POST":
-
-        form = FarmForm(
-            request.POST,
-            request.FILES,
-            instance=farm
-        )
-
-        if form.is_valid():
-
-            form.save()
-
-            messages.success(
-                request,
-                "Farm updated successfully."
-            )
-
-            return redirect("farms:my_farm")
-
-        else:
-
-            messages.error(
-                request,
-                "Please fix the errors below and resubmit."
-            )
-
-    else:
-
-        form = FarmForm(
-            instance=farm
-        )
-
-    return render(
-        request,
-        "farms/edit_farm.html",
-        {
-            "form": form
-        }
-    )
+]
